89988f4d7cc16e082846f88685e96ca2f53b7573
Zack Wilder <zwilder@gmail.com>
2021-03-17 22:12:15 -0400

    make blockchain

---
+++ dev/blockchain.js
@@ -0,0 +1,54 @@
+import sha256 from "sha256";
+
+export class Blockchain {
+  constructor() {
+    this.chain = [];
+    // q for pending transactions that haven't been mined
+    this.pendingTransactions = [];
+    createNewBlock(0, "0000000", "0");
+  }
+
+  // mine the block
+  createNewBlock(nonce, prevBlockHash, hash) {
+    const newBlock = {
+      index: this.chain.length + 1,
+      timestamp: Date.now(),
+      transactions: this.pendingTransactions,
+      nonce,
+      prevBlockHash,
+      hash,
+    };
+
+    this.chain.push(newBlock);
+    this.pendingTransactions = [];
+    return newBlock;
+  }
+
+  getLastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  createNewTransaction(amount, sender, recipient) {
+    const next = { amount, sender, recipient };
+    this.pendingTransactions.push(next);
+
+    return this.getLastBlock().index + 1;
+  }
+
+  hashBlock(prevBlockHash, currBlockData, nonce) {
+    const dataAsString = prevBlockHash + JSON.stringify(currBlockData) + nonce;
+    const hash = sha256(dataAsString);
+    return hash;
+  }
+
+  proofOfWork(prevBlockHash, currBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(prevBlockHash, currBlockData, nonce);
+    while (hash.slice(0, 4) !== "0000") {
+      nonce++;
+      hash = this.hashBlock(prevBlockHash, currBlockData, nonce);
+    }
+
+    return nonce;
+  }
+}
---
+++ dev/test.js
@@ -0,0 +1,16 @@
+import { Blockchain } from "./blockchain.js";
+
+const bc = new Blockchain();
+
+const pBH = "1324";
+const currBlockData = [
+  {
+    amount: 234,
+    from: "hey",
+    to: "yo",
+  },
+];
+
+const nonce = 1234;
+
+console.log(bc.proofOfWork(pBH, currBlockData));
